31ca6e91dda223fe2f5d6d6e0c731bf818416b27
FrankLikuohao <frank.likuohao@gmail.com>
2017-03-14 19:55:44 +0800

    上課備份

---
+++ CS280-Lecture4-Example/Example01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Status response = Status.Success;
+            switch (response)
+            {
+                case Status.Success;
+                    //你要寫的程式
+                    Console.WriteLine("狀態是成功");
+                    Console.WriteLine((int)response);
+                    break;
+
+                case Status.Success;
+                    //你要寫的程式
+                    Console.WriteLine("狀態是成功");
+                    Console.WriteLine(response);
+                    break;
+
+                case Status.Error;
+                    //你要寫的程式
+                    Console.WriteLine("狀態不明,所以什麼都不做");
+                    Console.WriteLine(response);
+                    break;
+            }
+        }
+    }
+}
---
+++ CS280-Lecture4-Example/Example01/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    class enum Status :long
+    {
+        Success = 1,Error
+    }
+}
